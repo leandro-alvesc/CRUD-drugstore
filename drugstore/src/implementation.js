39c1ca5653a7e2b62651fc99cce07167cfba13a5
Leandro Alves <leandrofelipecunha@hotmail.com>
2021-12-31 19:27:39 -0300

    feat: implement host drugstore methods

--- drugstore/src/implementation.js
+++ drugstore/src/implementation.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 
 const drugstores = [
   {
-    id: uuidv4(),
+    id: "219fbd56-8cf1-4cfb-98f4-6540f35929a6",
     logo: "https://image.shutterstock.com/image-vector/pharmacy-logo-design-drugstore-pill-600w-1913369134.jpg",
     name: "FarmaSampa",
     cnpj: "04.111.795/0001-66",
@@ -13,7 +13,7 @@ const drugstores = [
     phone_number: "(11) 3807-9445",
   },
   {
-    id: uuidv4(),
+    id: "6b9887a1-778c-4a8c-884a-fb8276d38d7e",
     logo: "https://image.shutterstock.com/image-vector/cross-drugstore-medical-pharmacy-logo-600w-706509610.jpg",
     name: "DrogaHenri",
     cnpj: "80.274.558/0001-02",
@@ -23,7 +23,7 @@ const drugstores = [
     phone_number: "(91) 3741-8368",
   },
   {
-    id: uuidv4(),
+    id: "d7f3041e-2a3a-4fb1-adba-66873d435581",
     logo: "https://image.shutterstock.com/image-vector/drug-choicedrugstore-logovector-logo-template-600w-328275176.jpg",
     name: "Drogaria da Cidade",
     cnpj: "47.247.785/0001-65",
@@ -34,12 +34,43 @@ const drugstores = [
   },
 ];
 
+const hosts = [
+  {
+    id: "219fbd56-8cf1-4cfb-98f4-6540f35929a6",
+    drugstores: ["d7f3041e-2a3a-4fb1-adba-66873d435581"],
+  },
+];
+
 const findDrugstore = (id) => {
   return drugstores.find((d) => d.id === id);
 };
 
 const findDrugstoreIndex = (id) => {
   return drugstores.findIndex((d) => d.id === id);
+};
+
+const findHostDrugstore = (id) => {
+  return hosts.find((h) => h.id === id);
+};
+
+const findHostDrugstoreIndex = (id) => {
+  return hosts.findIndex((h) => h.id === id);
+};
+
+const findDrugstoreInHostIndex = (drugstores, id) => {
+  return drugstores.findIndex((d) => d === id);
+};
+
+const invalidDrugstores = (drugstores) => {
+  let invalid = [];
+
+  drugstores.forEach((d) => {
+    if (!findDrugstore(d)) {
+      invalid.push(d);
+    }
+  });
+
+  return invalid;
 };
 
 const implementation = {
@@ -72,9 +103,11 @@ const implementation = {
       currentDrugstore.name = request.name || currentDrugstore.name;
       currentDrugstore.cnpj = request.cnpj || currentDrugstore.cnpj;
       currentDrugstore.address = request.address || currentDrugstore.address;
-      currentDrugstore.business_hours = request.business_hours || currentDrugstore.business_hours;
+      currentDrugstore.business_hours =
+        request.business_hours || currentDrugstore.business_hours;
       currentDrugstore.manager = request.manager || currentDrugstore.manager;
-      currentDrugstore.phone_number = request.phone_number || currentDrugstore.phone_number;
+      currentDrugstore.phone_number =
+        request.phone_number || currentDrugstore.phone_number;
       callback(null, currentDrugstore);
     } else {
       callback({ code: grpc.status.NOT_FOUND, details: "Not found" });
@@ -89,6 +122,115 @@ const implementation = {
       callback(null, {});
     } else {
       callback({ code: grpc.status.NOT_FOUND, details: "Not found" });
+    }
+  },
+
+  getAllHosts: (_, callback) => {
+    callback(null, { hosts });
+  },
+
+  getHostById: (call, callback) => {
+    let hostDrugstore = findHostDrugstore(call.request.id);
+
+    hostDrugstore
+      ? callback(null, hostDrugstore)
+      : callback({ code: grpc.status.NOT_FOUND, details: "Not found" });
+  },
+
+  insertHost: (call, callback) => {
+    let hostDrugstore = call.request;
+    let drugstores = hostDrugstore.drugstores;
+
+    hostDrugstore.drugstores = drugstores ? drugstores : [];
+
+    let invalid = invalidDrugstores(hostDrugstore.drugstores);
+
+    if (!findDrugstore(hostDrugstore.id)) {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: "Drugstore not found",
+      });
+    } else if (invalid.length) {
+      callback({
+        code: grpc.status.INVALID_ARGUMENT,
+        details: `${invalid}: not found`,
+      });
+    } else if (findHostDrugstore(hostDrugstore.id)) {
+      callback({
+        code: grpc.status.ALREADY_EXISTS,
+        details: "Host already exists",
+      });
+    } else if (hostDrugstore.drugstores.length > 3) {
+      callback({
+        code: grpc.status.INVALID_ARGUMENT,
+        details: "Limit: 3 drugstores per host",
+      });
+    } else {
+      hosts.push(hostDrugstore);
+      callback(null, hostDrugstore);
+    }
+  },
+
+  removeHost: (call, callback) => {
+    let currentHostIndex = findHostDrugstoreIndex(call.request.id);
+
+    if (currentHostIndex != -1) {
+      hosts.splice(currentHostIndex, 1);
+      callback(null, {});
+    } else {
+      callback({ code: grpc.status.NOT_FOUND, details: "Not found" });
+    }
+  },
+
+  insertDrugstoreToHost: (call, callback) => {
+    let request = call.request;
+    let currentHost = findHostDrugstore(request.id);
+
+    let invalid = invalidDrugstores([request.drugstore]);
+
+    let drugstoreIndex = findDrugstoreInHostIndex(
+      currentHost.drugstores,
+      request.drugstore
+    );
+
+    if (currentHost.drugstores.length === 3) {
+      callback({
+        code: grpc.status.FAILED_PRECONDITION,
+        details: "Host drugstore limit reached",
+      });
+    } else if (invalid.length) {
+      callback({
+        code: grpc.status.INVALID_ARGUMENT,
+        details: `${invalid}: not found`,
+      });
+    } else if (drugstoreIndex != -1) {
+      callback({
+        code: grpc.status.ALREADY_EXISTS,
+        details: `${request.drugstore}: already exists`,
+      });
+    } else {
+      currentHost.drugstores.push(request.drugstore);
+      callback(null, currentHost);
+    }
+  },
+
+  removeDrugstoreFromHost: (call, callback) => {
+    let request = call.request;
+    let currentHost = findHostDrugstore(request.id);
+
+    let drugstoreIndex = findDrugstoreInHostIndex(
+      currentHost.drugstores,
+      request.drugstore
+    );
+
+    if (drugstoreIndex != -1) {
+      currentHost.drugstores.splice(drugstoreIndex, 1);
+      callback(null, currentHost);
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: "Drugstore ID not found in host",
+      });
     }
   },
 };
