8f78d6c9f13634ca7e97429703661cea43c561d7
Leandro Alves <leandrofelipecunha@hotmail.com>
2021-12-31 01:55:20 -0300

    feat: add express api

---
+++ api/src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import router from "./routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("/", router);
+
+console.info("Server running at port 3333");
+app.listen(3333);
